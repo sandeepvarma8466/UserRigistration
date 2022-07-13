7abe1ad6f5071ce0c21d146b157474a84f39dde6
sandeepvarma8466 <svarma852@gmail.com>
2022-07-13 17:01:40 +0530

    Welcomr to user rigistration problem

---
+++ src/main/java/com/blz/regex/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.blz.regex;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Rigistration problem");
+    }
+}

a52e9bbb4e34bad2e5f6f48118f779ac6bd99a0b
sandeepvarma8466 <svarma852@gmail.com>
2022-07-13 17:39:07 +0530

    User need to enter valid lastName

--- src/main/java/com/blz/regex/UserRegistration.java
+++ src/main/java/com/blz/regex/UserRegistration.java
@@ -6,11 +6,13 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     public static final Scanner sc = new Scanner(System.in);
-    public static final String FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+    public static final String FIRST_NAME = "^[A-Z][a-z]{2,}$";
+    public static final String LAST_NAME = "^[A-Z][a-z]{2,}$";
     public static void main(String[] args) {
         System.out.println("Enter the first name of user : ");
-        String name = sc.nextLine();
-        isValidFirstName(name);
+        isValidFirstName(sc.nextLine());
+        System.out.println("Enter the last name of user : ");
+        isValidLastName(sc.nextLine());
     }
     public static void isValidFirstName(String name) {
         try {
@@ -23,6 +25,17 @@ public class UserRegistration {
             System.out.println(e);
         }
     }
+    public static void isValidLastName(String name) {
+        try {
+            if (isValidInput(LAST_NAME, name)) {
+                System.out.println("Valid Last Name is: " + name);
+            } else {
+                throw new InvalidUserInformationException("Invalid Last Name");
+            }
+        } catch (InvalidUserInformationException e) {
+            System.out.println(e);
+        }
+    }
     private static boolean isValidInput(String regex, String userInput) {
         return Pattern.matches(regex,userInput);
     }
@@ -30,9 +43,7 @@ public class UserRegistration {
 class InvalidUserInformationException extends RuntimeException {
     public InvalidUserInformationException() {
         super();
-
     }
-
     public InvalidUserInformationException(String msg) {
         super(msg);
     }

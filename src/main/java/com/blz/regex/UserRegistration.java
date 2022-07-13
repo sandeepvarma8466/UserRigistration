9b87977040f975be94006131e2ba777da09dc946
sandeepvarma8466 <svarma852@gmail.com>
2022-07-13 17:31:02 +0530

    User need to enter a valid firstname

--- src/main/java/com/blz/regex/UserRegistration.java
+++ src/main/java/com/blz/regex/UserRegistration.java
@@ -1,7 +1,39 @@
 package com.blz.regex;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static final Scanner sc = new Scanner(System.in);
+    public static final String FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
     public static void main(String[] args) {
-        System.out.println("Welcome to User Rigistration problem");
+        System.out.println("Enter the first name of user : ");
+        String name = sc.nextLine();
+        isValidFirstName(name);
+    }
+    public static void isValidFirstName(String name) {
+        try {
+            if (isValidInput(FIRST_NAME, name)) {
+                System.out.println("Valid First Name is: " + name);
+            } else {
+                throw new InvalidUserInformationException("Invalid First Name");
+            }
+        } catch (InvalidUserInformationException e) {
+            System.out.println(e);
+        }
+    }
+    private static boolean isValidInput(String regex, String userInput) {
+        return Pattern.matches(regex,userInput);
+    }
+}
+class InvalidUserInformationException extends RuntimeException {
+    public InvalidUserInformationException() {
+        super();
+
+    }
+
+    public InvalidUserInformationException(String msg) {
+        super(msg);
     }
 }
